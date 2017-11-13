bed8d2959daaf8d7d9aa1801cc6361c9b991a004
craiel <craieldev@gmail.com>
2017-11-13 17:50:53 -0500

    component frame

---
+++ VFXCore.cs
@@ -0,0 +1,20 @@
+﻿namespace Assets.Scripts.Craiel.VFX
+{
+    using Contracts;
+    using Essentials.UnityComponent;
+
+    public class VFXCore
+    {
+        // -------------------------------------------------------------------
+        // Constructor
+        // -------------------------------------------------------------------
+        static VFXCore()
+        {
+            new UnityComponentConfigurator<IVFXConfig>().Configure();
+        }
+
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+    }
+}
